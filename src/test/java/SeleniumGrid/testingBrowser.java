cfed6fb73c39c8937cec1e53813ae27d42ff42b4
RethnaU <rethna2613@gmail.com>
2025-09-14 09:13:10 +0530

    Assignment done

--- src/test/java/SeleniumGrid/testingBrowser.java
+++ src/test/java/SeleniumGrid/testingBrowser.java
@@ -1,9 +1,12 @@
 package SeleniumGrid;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -11,16 +14,31 @@ public class testingBrowser {
 	public WebDriver driver;
 	@Parameters({"browser"})
   @Test
-  public void CrossTesting(String browser) 
+  public void CrossTesting(String browser) throws MalformedURLException, InterruptedException 
   {
-	  if(browser.equals("Chrome")) {
-		  driver= new ChromeDriver();
-	  }else if(browser.equals("Firefox")) {
-		  driver= new FirefoxDriver();
-	  }else if(browser.equals("Edge")) {
-		  driver = new EdgeDriver();
+		 System.out.println("Remote connection is starting.....");
+	  if(browser.equals("Chrome"))
+	  {
+		  ChromeOptions options = new ChromeOptions();
+		  driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);
+		  System.out.println("Session created on Chrome");
+		  
+	  }else if(browser.equals("Firefox")) 
+	  {
+		  FirefoxOptions options = new FirefoxOptions();
+		  driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);  
+		  System.out.println("Session created on Firefox");
 	  }
+	  else if(browser.equals("Edge")) {
+		  EdgeOptions options = new EdgeOptions();
+		  driver= new RemoteWebDriver(new URL("http://localhost:4444"),options);
+		  System.out.println("Session created on Edge");
+	  }
+	  System.out.println("Remote connection is established");
+	  Thread.sleep(10000);
 	  driver.get("https://www.amazon.in");
+	  Thread.sleep(5000);
 	  System.out.println("Title is :"+driver.getTitle());
+	  driver.quit();
   }
 }
